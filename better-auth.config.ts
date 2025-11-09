961da6905fa953a9cc56ca91903dcca6329f63f6
Winterest | M. Adam Yudistira <yudistiraadam3@gmail.com>
2025-11-09 09:59:36 +0700

    setup better auth

---
+++ better-auth.config.ts
@@ -0,0 +1,24 @@
+/**
+ * Better Auth CLI configuration file
+ *
+ * Docs: https://www.better-auth.com/docs/concepts/cli
+ */
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { betterAuth } from 'better-auth';
+import { betterAuthOptions } from './src/lib/better-auth/options';
+
+import * as schema from "./src/db/schema";
+
+const { DATABASE_URL, BETTER_AUTH_URL, BETTER_AUTH_SECRET } = process.env;
+
+const sql = neon(DATABASE_URL!);
+const db = drizzle(sql);
+
+export const auth: ReturnType<typeof betterAuth> = betterAuth({
+  ...betterAuthOptions,
+  database: drizzleAdapter(db, { provider: 'pg', schema }),
+  baseURL: BETTER_AUTH_URL,
+  secret: BETTER_AUTH_SECRET,
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Hono } from 'hono'
+import { auth } from './lib/better-auth';
 
-const app = new Hono()
+const app = new Hono<{ Bindings: CloudflareBindings }>();
+
+app.on(['GET', 'POST'], '/api/*', (c) => {
+  return auth(c.env).handler(c.req.raw);
+});
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
